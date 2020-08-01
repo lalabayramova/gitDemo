09bc7f4db00f5322d7e0155171c4f58155675e03
lalabayramova551 <b.lala95@yahoo.com>
2020-08-01 10:51:42 -0500

    update HappyB20.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -2,5 +2,6 @@ public class HelloB20 {
     public static void main(String[] args) {
 
         System.out.println("Helllo B20, it is my pleasure to be with you!");
+        System.out.println("Lala is happy");
     }
 }
